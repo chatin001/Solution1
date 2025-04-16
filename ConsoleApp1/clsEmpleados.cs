9747deb8a5d2bd89106793ff3ec47f58ee7e5df9
chatin001 <148296751+chatin001@users.noreply.github.com>
2025-04-15 22:09:21 -0500

    Cremos Metodos Get-Set

--- ConsoleApp1/clsEmpleados.cs
+++ ConsoleApp1/clsEmpleados.cs
@@ -8,11 +8,53 @@ namespace ConsoleApp1
 {
     public class clsEmpleados
     {
-        public string nombre;
-        public string edad;
-        public string genero;
-        public string sueldo;
+        private string _nombre;
 
+        public string NombrePublico
+        {
+            //devuelve
+            get { return _nombre; }
+
+            //Define 
+            set { _nombre = value; }
+        }
+
+
+        private string _edad;
+
+        public string EdadPublico
+        {
+            //devuelve
+            get { return _edad; }
+
+            //Define 
+            set { _edad = value; }
+        }
+
+
+
+        private string _genero;
+
+        public string GeneroPublico
+        {
+            //devuelve
+            get { return _genero; }
+
+            //Define 
+            set { _genero = value; }
+        }
+
+
+        private string _sueldo;
+
+        public string SueldoPublico
+        {
+            //devuelve
+            get { return _sueldo; }
+
+            //Define 
+            set { _sueldo = value; } 
+        }
 
     }
 }
